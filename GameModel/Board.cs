ff413121df4315c2f6a5dfd392bc90ccf46b142d
Royga2 <galiliroy@gmail.com>
2023-08-24 17:42:42 +0300

    first commit

---
+++ GameModel/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel
+{
+    class Board
+    {
+        public Cell[,] m_Cells { get; private set; }
+        private readonly int r_Rows;
+        private readonly int r_Cols;
+
+        public Board(int i_Rows, int i_Cols)
+        {
+            this.r_Rows = i_Rows;
+            this.r_Cols = i_Cols;
+
+            m_Cells = new Cell[i_Rows, i_Cols];
+
+            for(int i = 0; i < i_Rows; i++)
+            {
+                for(int j = 0; j < i_Cols; j++)
+                {
+                    m_Cells[i, j] = new Cell(i, j);
+                }
+            }
+
+        }
+    }
+}
---
+++ GameModel/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameModel
+{
+    class Cell
+    {
+        public bool IsAlive { get; set; }
+        private readonly int r_Row;
+        private readonly int r_Col;
+
+        public Cell(int i_Row, int i_Col)
+        {
+            r_Row = i_Row;
+            r_Col = i_Col;
+        }
+
+        public int Row => r_Row;
+        public int Col => r_Col;
+    }
+}
